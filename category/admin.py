5d7eb3f1843a3bacddde2b21978f1c3eb756817d
perammadhava <madhava@rushelysian.com>
2023-12-03 22:50:15 +0530

    added pagination and cart functionality

--- category/admin.py
+++ category/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import category
 
 # Register your models here.
-
-admin.site.register(category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name','slug')
+admin.site.register(category,CategoryAdmin)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -128,6 +132,9 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
